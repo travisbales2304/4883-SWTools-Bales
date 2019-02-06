4954249a6ae5d73e8b52fed12a9e0c45af63104a
travisbales2304 <gligglebuck@yahoo.com>
2019-02-06 11:48:02 -0600

    Create scrape_game_data.py

---
+++ Assignments/A03/scrape_game_data.py
@@ -0,0 +1,18 @@
+from beautifulscraper import BeautifulScraper
+from pprint import pprint
+import urllib
+from urllib.request import urlretrieve
+from time import sleep
+import json
+
+scraper = BeautifulScraper()
+
+
+
+for x in range(1970,2018):
+        f = open("Post %s.txt"%(str(x)),"r")
+        lines = []
+        for line in f:
+                id = line.strip('\n')
+                url="http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json" % (id,id)
+                urllib.request.urlretrieve(url,'%s.json'%(id))

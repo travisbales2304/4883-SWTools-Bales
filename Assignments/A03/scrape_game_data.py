d06c99d3941928c42abd34a8b9d52378000ac1d3
travisbales2304 <gligglebuck@yahoo.com>
2019-02-12 20:20:20 -0800

    Update scrape_game_data.py

--- Assignments/A03/scrape_game_data.py
+++ Assignments/A03/scrape_game_data.py
@@ -9,9 +9,16 @@ scraper = BeautifulScraper()
 
 
 #opens each file by year with game ids
-for x in range(1970,2018):
-        f = open("Post %s.txt"%(str(x)),"r")
-        for line in f:
-                id = line.strip('\n')
-                url="http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json" % (id,id)
-                urllib.request.urlretrieve(url,'%s.json'%(id))
+for x in range(2009,2019):
+    f = open("Post %s.txt"%(str(x)),"r")
+    for line in f:
+        id = line.strip('\n')
+        url="http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json" % (id,id)
+        urllib.request.urlretrieve(url,'%s.json'%(id))
+    f.close()
+    f = open("REG %s.txt"%(str(x)),"r")
+    for line in f:
+        id = line.strip('\n')
+        url="http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json" % (id,id)
+        urllib.request.urlretrieve(url,'%s.json'%(id))
+    f.close()

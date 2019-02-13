d07a3ff8caa3355f81c215c2c3b9552930244180
travisbales2304 <gligglebuck@yahoo.com>
2019-02-12 20:21:22 -0800

    Update calculate_stats.py

--- Assignments/A03/calculate_stats.py
+++ Assignments/A03/calculate_stats.py
@@ -1,1 +1,88 @@
 
+import json
+import os
+import sys
+from pprint import pprint
+
+def MostTeams():
+    pass
+def MostTeamsYR():
+    pass
+def MostYrdsRushedForLoss():
+    pass
+def MostRushesForLoss():
+    pass
+def MostPassesForLoss():
+    pass
+def TeamMostPenalties():
+    pass
+def TeamMostYrdsPenalties():
+    pass
+def AvgPlaysPerGame():
+    pass
+def LongestFG():
+    pass
+def MostFG():
+    pass
+def MostFailedFG():
+    pass
+def MostDroppedPasses():
+    pass
+
+
+
+
+
+print('Name: Travis Bales \nAssignment: A03 - Nfl Stats \nDate: 2/12/2019\n\n')
+
+print('Find the player(s) that played for the most teams.\nAnswer:\n')
+#anser to question
+print('\n\n')
+
+print('Find the player(s) that played for multiple teams in one year.\nAnswer:\n')
+#anser to question
+print('\n\n')
+
+print('Find the player(s) that had the most yards rushed for a loss.\nAnswer:\n')
+#anser to question
+print('\n\n')
+
+print('Find the player(s) that had the most rushes for a loss.\nAnswer:\n')
+#anser to question
+print('\n\n')
+
+print('Find the player(s) with the most number of passes for a loss.\nAnswer:\n')
+#anser to question
+print('\n\n')
+
+print('Find the team with the most penalties.\nAnswer:\n')
+#anser to question
+print('\n\n')
+
+print('Find the team with the most yards in penalties.\nAnswer:\n')
+#anser to question
+print('\n\n')
+
+print('Find the correlation between most penalized teams and games won / lost.\nAnswer:\n')
+#anser to question
+print('\n\n')
+
+print('Average number of plays in a game.\nAnswer:\n')
+#anser to question
+print('\n\n')
+
+print('Longest field goal.\nAnswer:\n')
+#anser to question
+print('\n\n')
+
+print('Most field goals.\nAnswer:\n')
+#anser to question
+print('\n\n')
+
+print('Most missed field goals.\nAnswer:\n')
+#anser to question
+print('\n\n')
+
+print('Most dropped passes\nAnswer:\n')
+#anser to question
+print('\n\n')
